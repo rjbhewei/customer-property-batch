553ed03d416c67a83195d610b5d7463937aaa579
rjbhewei <rjbhewei@gmail.com>
2016-11-16 14:37:22 +0800

    grpc-go 测试

---
+++ TestGrpcClient.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"github.com/rjbhewei/customer-property-batch/common"
+	"encoding/json"
+	"google.golang.org/grpc"
+	pb "github.com/rjbhewei/customer-property-batch/helloworld"
+	"golang.org/x/net/context"
+)
+
+var (
+	mylog = common.Log()
+	bean = &common.BatchUpdateBean{
+		Customers: []string{"Customers1", "Customers2"},
+		Platform :"Platform",
+		TenantId : "TenantId",
+		Value :"Value",
+		PropertyId:"PropertyId",
+	}
+)
+
+const (
+	address = "localhost:50051"
+)
+
+func init() {
+	j, _ := json.Marshal(bean)
+	mylog.Info(string(j))
+}
+
+func main() {
+	conn, err := grpc.Dial(address, grpc.WithInsecure())
+	if err != nil {
+		mylog.Errorf("did not connect: %v", err)
+	}
+
+	defer conn.Close()
+	c := pb.NewEncryptClient(conn)
+
+	r, err := c.ToEncrypt(context.Background(), &pb.EncryptRequest{
+		Customers: bean.Customers,
+		Platform: bean.Platform,
+		TenantId: bean.TenantId,
+		Value: bean.Value,
+		PropertyId: bean.PropertyId,
+	})
+
+	if err != nil {
+		mylog.Errorf("rpc encrypt error: %v", err)
+	}
+	j, _ := json.Marshal(r)
+	mylog.Info("EncryptReply : %s", string(j))
+}
+
---
+++ TestGrpcServer.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"github.com/rjbhewei/customer-property-batch/common"
+	"golang.org/x/net/context"
+	pb "github.com/rjbhewei/customer-property-batch/helloworld"
+	"net"
+	"google.golang.org/grpc"
+)
+
+var (
+	mylog = common.Log()
+)
+
+const (
+	port = ":50051"
+)
+
+type server struct{}
+
+func (s *server) ToEncrypt(ctx context.Context, in *pb.EncryptRequest) (*pb.EncryptReply, error) {
+	return &pb.EncryptReply{
+		Customers: in.Customers,
+		Platform: in.Platform + "<-",
+		TenantId: in.TenantId + "<-",
+		Value: in.Value + "<-",
+		PropertyId: in.PropertyId + "<-",
+	}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		mylog.Errorf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterEncryptServer(s, &server{})
+	if err := s.Serve(lis); err != nil {
+		mylog.Errorf("failed to serve: %v", err)
+	}
+}
