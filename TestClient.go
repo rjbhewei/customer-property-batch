27284f48e8776b900dd3801a53409b0d5fd65f6a
rjbhewei <rjbhewei@gmail.com>
2016-11-21 15:24:02 +0800

    添加consul简单的服务发现

--- TestClient.go
+++ TestClient.go
@@ -1,5 +1,6 @@
 package main
 
+
 import (
 	"github.com/rjbhewei/customer-property-batch/common"
 )
@@ -8,4 +9,5 @@ var mylog = common.Log()
 
 func main() {
 	mylog.Info(common.ConsulService("10.117.8.138:8500","/cryptserver/1.0"))
+	mylog.Info(common.EtcdService("http://172.18.21.62:2379","/service/local/platform/qa/cryptserver/1.0"))
 }
